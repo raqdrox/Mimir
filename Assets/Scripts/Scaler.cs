7a699095968de34427480cae9ad4f5d78ae4ad2f
ParaD0X <51704206+frosty333@users.noreply.github.com>
2020-12-07 21:36:01 +0530

    Dev Build 9 Optimised
    Fixed high texture memory usage

---
+++ Assets/Scripts/Scaler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaler : MonoBehaviour
+{
+    void Start()
+    {
+        float width = ScreenSize.GetScreenToWorldWidth;
+        transform.localScale = Vector3.one * width;
+    }
+}
