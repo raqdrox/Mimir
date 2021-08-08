3976739c37487fc5b1863ab69784ca1f6e911f28
ParaD0X <51704206+frosty333@users.noreply.github.com>
2021-08-08 18:20:37 +0530

    cleanup

---
+++ Assets/Scripts/Phone/Settings.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    [Header("Theme")]
+    public string MessageThemeName;
+    public string RineThemeName;
+    [Header("Transition")]
+    public float TransSpeed;
+    [Header("Audio")]
+    public int VolumeLvl;
+}
---
+++ Assets/Scripts/Phone/ThemeScript.cs
@@ -0,0 +1,78 @@
+﻿using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ThemeScript : MonoBehaviour
+{
+    public GameObject pnlObj;
+    public GameObject[] gobjlist;
+    public bool LoadScript = false;
+    public void Awake()
+    {
+        if(LoadScript)
+        {
+            Load();
+        }
+    }
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/prefs.txt"))
+        {
+            string saveString = File.ReadAllText(Application.persistentDataPath + "/prefs.txt");
+            SaveObject saveObject = JsonUtility.FromJson<SaveObject>(saveString);
+            pnlObj.GetComponent<Image>().color = saveObject.dm_PanelCol;
+            foreach(GameObject txtObj in gobjlist)
+            {
+                txtObj.GetComponent<Text>().color = saveObject.dm_Txt;
+            }
+
+        }
+        else
+        {
+            Debug.Log("No File");
+        }
+
+    }
+    public void Save(bool theme)
+    {
+        if (File.Exists(Application.persistentDataPath + "/prefs.txt"))
+        {
+            string saveString = File.ReadAllText(Application.persistentDataPath + "/prefs.txt");
+            SaveObject saveObject = JsonUtility.FromJson<SaveObject>(saveString);
+            if(theme)
+            {
+                saveObject.dm_PanelCol = Color.black;
+                saveObject.dm_Txt = Color.white;
+
+            }
+            else
+            {
+                saveObject.dm_PanelCol = Color.white;
+                saveObject.dm_Txt = Color.black;
+
+            }
+            string json = JsonUtility.ToJson(saveObject);
+            File.WriteAllText(Application.persistentDataPath + "/prefs.txt", json);
+        }
+        else
+        {
+            SaveObject saveObject = new SaveObject
+            {
+                dm_PanelCol = Color.white,
+                dm_Txt = Color.black,
+            };
+            string json = JsonUtility.ToJson(saveObject);
+            File.WriteAllText(Application.persistentDataPath + "/prefs.txt", json);
+        }
+
+    }
+
+}
+
+public class SaveObject
+{
+    public Color dm_PanelCol;
+    public Color dm_Txt;
+
+}
